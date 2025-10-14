e1325af79c293e50d791155be8584263799e72ab
eduardo bandeira <fustecdev@gmail.com>
2025-10-14 17:59:03 -0300

    nota de regras

--- index.js
+++ index.js
@@ -5,6 +5,41 @@ import { audio } from './parasha.js';
 const menu = document.querySelector(".glow-on-hover")
 const optio =document.querySelector(".optio")
 const audio1 = document.querySelector(".audio")
+const btn = document.querySelector(".iputbtn")
+const inicio = document.querySelector(".inicio")
+const norma = document.querySelector(".norma")
+
+const checkbox = document.getElementById("myCheckbox");
+var isChecked = ""
+var observ = setInterval(() => {
+    isChecked = checkbox.checked;
+    if (isChecked) {
+  // o checkbox está marcado
+ 
+  btn.style.display="block"
+  clearInterval(observ)
+
+} else {
+  // o checkbox não está marcado
+  
+}
+}, 1000);
+
+btn.addEventListener("click", ()=>{
+    inicio.style.display="block"
+    norma.style.display="none"
+    parasha.map((index) =>{
+var p = document.createElement("p")
+p.textContent=parasha[cont]
+
+document.body.appendChild(p)
+cont++
+
+
+}) 
+
+})
+
 
 audio1.src=audio
 
@@ -18,12 +53,6 @@ optio.addEventListener("mouseleave",() =>{
 })
 
 var cont = 0
-parasha.map((index) =>{
-var p = document.createElement("p")
-p.textContent=parasha[cont]
-
-document.body.appendChild(p)
-cont++
 
 
-})
+
